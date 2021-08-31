ed75a49b41a716ceef71a9e5548312672d1474f5
Peter <Walsh9636@gmail.com>
2021-08-30 20:16:36 -0400

    Started working on projects page

--- js/page_builder.js
+++ js/page_builder.js
@@ -17,27 +17,41 @@ function build_navbar(navbar_options) {
     navbar_box.id = "navbar";
     navbar_box.classList = ["container"];
 
-    navbar_row.classList = ["row justify-content-start"];
+    navbar_row.classList = ["container d-flex"];
 
     for (let i = 0; i < navbar_options.length; i++) {
         var option = document.createElement("div");
-        option.classList = ["col bg-primary"];
+        option.classList = ["col bg-light"];
 
         option.append(build_link(navbar_options[i].name, navbar_options[i].link));
+        option.append(build_icon(navbar_options[i].icon));
         navbar_row.append(option);
     }
+
+
 
     return navbar_box.appendChild(navbar_row);
 }
 
-function build_link(text, link) {
+function build_link(text, link, icon) {
     var my_link = document.createElement("a");
-    my_link.classList = ["text-light"]
-
+    my_link.classList = ["btn btn-primary"];
+    my_link.role = "button";
     my_link.href = link;
     my_link.append(text);
+
+
     return my_link;
 }
+
+function build_icon(icon) {
+    var link_icon = document.createElement("span");
+    link_icon.classList = ["fas " + icon];
+    return link_icon;
+}
+
+
+
 
 
 
---
+++ js/project_page.js
@@ -0,0 +1,11 @@
+function build_project_page(project_data) {
+
+}
+
+function build_project_row(project_data) {
+
+}
+
+function build_project_description(project_data) {
+    
+}
