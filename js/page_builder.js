1ae741559867fe6fb92cf43d1fc6020c25b442aa
Peter <Walsh9636@gmail.com>
2021-09-03 21:36:47 -0400

    Finished the navbar

--- js/page_builder.js
+++ js/page_builder.js
@@ -1,7 +1,6 @@
-// This file will contain all of the things that need to be built for each page
-// that is a part of the website. Like navigation bar, etc.
-function load_navbar(data_path) {
-    var my_json = $.getJSON(data_path, (json) => {
+
+function build_home(data_path) {
+    let my_json = $.getJSON(data_path, (json) => {
         add("body", build_navbar(json.navigation.options)); // Adds navigation bar to the page
     });
 }
@@ -11,43 +10,37 @@ function add(location, element) {
 }
 
 function build_navbar(navbar_options) {
-    var navbar_box = document.createElement("div");
-    var navbar_row = document.createElement("div");
+    let navbar_box = document.createElement("div");
 
     navbar_box.id = "navbar";
-    navbar_box.classList = ["container"];
-
-    navbar_row.classList = ["container d-flex"];
+    navbar_box.classList = ["navbar container-fluid justify-content-start d-flex bg-dark"];
 
     for (let i = 0; i < navbar_options.length; i++) {
-        var option = document.createElement("div");
-        option.classList = ["col bg-light"];
-
-        option.append(build_link(navbar_options[i].name, navbar_options[i].link));
-        option.append(build_icon(navbar_options[i].icon));
-        navbar_row.append(option);
+        navbar_box.append(build_link(navbar_options[i].name, 
+                                 navbar_options[i].link,
+                                 navbar_options[i].icon));
     }
 
-
-
-    return navbar_box.appendChild(navbar_row);
+    return navbar_box
 }
 
-function build_link(text, link, icon) {
-    var my_link = document.createElement("a");
-    my_link.classList = ["btn btn-primary"];
+function build_link(text, link, icon_class) {
+    let my_link = document.createElement("a");
+    my_link.classList = ["btn btn-dark text-light"];
     my_link.role = "button";
     my_link.href = link;
-    my_link.append(text);
+
+    my_link.append(build_icon(icon_class))
+    my_link.append(" " + text);
 
 
     return my_link;
 }
 
-function build_icon(icon) {
-    var link_icon = document.createElement("span");
-    link_icon.classList = ["fas " + icon];
-    return link_icon;
+function build_icon(icon_class) {
+    let icon = document.createElement("i");
+    icon.classList = ["fas " + icon_class];
+    return icon;
 }
 
 
